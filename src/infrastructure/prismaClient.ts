12dd40c558888df0832c265b7b18652af7b3fdd5
Ilham Suryana <ilham.suryana@dealls.com>
2026-03-27 05:21:50 +0700

    Feat:: Creating set up migration

--- src/infrastructure/prismaClient.ts
+++
@@ -1,4 +0,0 @@
-// Auto-generated prismaClient stub; will work after you run 'prisma generate'.
-import { PrismaClient } from '../generated/prisma/client.js';
-
-export const prisma = new PrismaClient();
